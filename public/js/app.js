a6ddf455f1d13f5ae880300343cea0aca203930a
Sanju <sanjeevkumar625020@gmail.com>
2021-02-02 15:42:57 +0530

    Removed unnecessary Console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,3 @@
-console.log('This is client side javascript')
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-
-// }
-// )
-
-
-
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
